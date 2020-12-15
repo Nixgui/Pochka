6791e08161a256465880121216c83ff30063e96b
Nixgui <robert.ecward@gmail.com>
2020-12-15 20:30:22 +0200

    Добвление функции
    Добавление функции изучения слов "Переведи слово"

--- Func.py
+++ Func.py
@@ -1,15 +1,16 @@
-def rus_eng(f1,f2):
+import random as r
+def rus_eng(f1,f2): #Функция перевода слов
     ruen=input("Введите слово которое хотите перевести => ")
     if ruen in f1:
-        print(f"{ruen} - {f2[f1.index(ruen)]}")
+        print(f"{ruen} - {f2[f1.index(ruen)]}") #Вывод Русский - Английский
     elif ruen in f2:
-        print(f"{ruen} - {f1[f2.index(ruen)]}")
+        print(f"{ruen} - {f1[f2.index(ruen)]}") #Вывод Английский - Русский
     else:
         print(f"{ruen} Отсуствует!")
         print("При отсутсвии слова, вы можете его добавить в меню! Выбрав 'Добавление перевода' ")
 
 
-def loe_fail(f):
+def loe_fail(f):  #Функция открытия и чтения файла
     fail=open(f,'r', encoding="utf-8-sig")
     mas=[]
     for rida in fail:
@@ -18,21 +19,69 @@ def loe_fail(f):
     print(mas)
     return mas
 
-def add_translate(f1,f2, ru, en):
+def add_translate(f1,f2, ru, en): #Функция для добавления слова в фаил
     with open(f1, 'a', encoding="utf-8-sig") as f1: f1.write(ru + '\n')
     with open(f2, 'a', encoding="utf-8-sig") as f2: f2.write(en + '\n')
 
-def recreate(l1,l2,l3,l4):
+def recreate(l1,l2,l3,l4): #Функция для поиска файла и последующей его замены
     ruen = input("Введите слово которое хотите найти => ")
     if ruen in l3:
         print(f"{ruen}")
-        with open(l1, encoding="utf-8-sig") as file_in:text = file_in.read()
+        with open(l1, encoding="utf-8-sig") as file_in:text = file_in.read() #Открытие файла и копирование файла в буфер
         nruen = input("Введи новое слово => ")
-        text = text.replace(ruen, nruen)
-        with open(l1, "w", encoding="utf-8-sig") as file_out:file_out.write(text)
+        text = text.replace(ruen, nruen) # Замена существуещего слова на новое
+        with open(l1, "w", encoding="utf-8-sig") as file_out:file_out.write(text) #Очистка файла и запись из буфера + изменённое слово
     elif ruen in l4:
         print(f"{ruen}")
         with open(l2, encoding="utf-8-sig") as file_in:text = file_in.read()
         nruen = input("Введи новое слово => ")
         text = text.replace(ruen, nruen)
         with open(l2, "w", encoding="utf-8-sig") as file_out:file_out.write(text)
+def znanie(f1,f2):
+    t=int(input("Вберите язык\n1-Русский\n2-Английский\n=> "))
+    if t==1:
+        kol=int(input("Сколько слов вы хотите перевести? => "))
+        koll=kol
+        bal=0
+        while kol>0:
+            try:
+                r1=r.choice(f1)
+                print(r1,'\n')
+                ii=f1.index(r1)
+                slovo=input("Введите перевод слова => ")
+                ii2=f2.index(slovo)
+                if ii==ii2:
+                    print("Правильно")
+                    print(".....................\n")
+                    bal+=1
+                    kol-=1
+                else:
+                    print("Неправильно")
+                    print(".....................\n")
+                    kol -= 1
+            except:
+                TypeError
+        print("Вы ответили на ","{:.0%}".format(bal/koll), "правильно")
+    if t==2:
+        kol = int(input("Сколько слов вы хотите перевести? => "))
+        koll = kol
+        bal = 0
+        while kol > 0:
+            try:
+                r2 = r.choice(f2)
+                print(r2, '\n')
+                ii = f2.index(r2)
+                slovo = input("Введите перевод слова => ")
+                ii2 = f1.index(slovo)
+                if ii == ii2:
+                    print("Правильно")
+                    print(".....................\n")
+                    bal += 1
+                    kol -= 1
+                else:
+                    print("Неправильно")
+                    print(".....................\n")
+                    kol -= 1
+            except:
+                TypeError
+        print("Вы ответили на ", "{:.0%}".format(bal / koll), "правильно")
--- Main.py
+++ Main.py
@@ -1,23 +1,23 @@
 from Func import *
+rus = []
+eng = []
+rus = loe_fail("rus.txt")
+eng = loe_fail("eng.txt")
 while 1:
-    a=int(input("1-Добавление перевода\n2-Перевод!\n3-Исправление слов\n0-Выход\n=> "))
+    a=int(input("1-Добавление перевода\n2-Перевод!\n3-Исправление слов\n4-Проверка знаний\n0-Выход\n=> "))
     if a==1:
         t=int(input("Какое количество слов хотите ввести? => "))
         for j in range(t):
-            ru=input("Руссое слово => ")
+            ru=input("Русское слово => ")
             en=input("Английское слово => ")
             add_translate("rus.txt","eng.txt",ru,en)
     if a==2:
-        rus=[]
-        eng=[]
-        rus=loe_fail("rus.txt")
-        eng=loe_fail("eng.txt")
         rus_eng(rus,eng)
     if a==0:
         break
     if a==3:
-        rus=[]
-        eng=[]
-        rus=loe_fail("rus.txt")
-        eng=loe_fail("eng.txt")
         recreate("rus.txt","eng.txt",rus,eng)
+    if a==4:
+        print("Слова будут появляться в хаотичном порядке как Русские, так и английские.")
+        print("Ваша задача написать правильный перевод слова")
+        znanie(rus,eng)
