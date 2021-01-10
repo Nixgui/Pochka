0f17ee91718b14779236650c1c8fc0528a546729
Nixgui <robert.ecward@gmail.com>
2021-01-10 22:15:07 +0200

    ф
    ф

--- Интерфейс/main.py
+++ Интерфейс/main.py
@@ -103,6 +103,24 @@ def babocgka():
     plt.plot(x16, y16, '-r', linewidth=1)
     plt.savefig("my_image.png")
     plt.show()
+def itsecurity():
+    fail = open("dannie.txt", "r")
+    mas1 = []
+    mas2 = []
+    for line in fail:
+        n = line.find(",")
+        mas1.append(line[0:n].strip())
+        mas2.append(int(line[n + 1:len(line)].strip()))
+    fail.close()
+
+    title = "Данные о ИТ безопасности"
+    plt.grid(True)
+
+    color_rectangle = np.random.rand(7, 3)
+    plt.barh(mas1, mas2, color=color_rectangle)
+
+    plt.show()
+itsecurity()
 ochki()
 zontik()
 babocgka()
