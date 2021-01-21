2766f6fd1c311eced1f7cd4bc3ec6440fe226c79
Nixgui <robert.ecward@gmail.com>
2021-01-21 12:35:46 +0200

    Database connection PROTOTYPE

---
+++ Own Project/Python/mysql_connect.py
@@ -0,0 +1,25 @@
+from mysql.connector import MySQLConnection, Error
+from DBconn import DBconf
+
+def connect():
+	db_config = DBconf()
+	try:
+		print("Connecting to MySQL database...")
+		connect = MySQLConnection(**db_config)
+		if connect.is_connected():
+			print("Connection established.")
+		else:
+			print("Connection failed.")
+	except Error as error:
+		print(error)
+	finally:
+		connect.close()
+		print("Connection Closed.")
+
+def iter_row(cursor, size=10):
+	while True:
+		rows = cursor.fetchmany(size)
+		if not rows:
+			break
+		for row in rows:
+			yield row
