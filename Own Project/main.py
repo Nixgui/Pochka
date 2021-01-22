d9c0dbfe9dabb1dcc42285cfe55c48d0d2858672
Nixgui <robert.ecward@gmail.com>
2021-01-22 16:02:01 +0200

    Update main.py

--- Own Project/main.py
+++ Own Project/main.py
@@ -1,11 +1,26 @@
-import pymysql
-from contextlib import closing
-from pymysql.cursors import DictCursor
-connection = pymysql.connect(
-    host='',
-    user='',
-    password='',
-    db='',
-    charset='',
-    cursorclass=DictCursor
-)
+from mysql.connector import MySQLConnection, Error
+from DBconn import DBconf
+def show_client(): 
+    try:
+        db_config=DBconf()
+        conn = MySQLConnection(**db_config)
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM books")
+
+        for row in iter_row(cursor, 10):
+            print(row)
+
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+def iter_row(cursor, size=10):
+    while True:
+        rows = cursor.fetchmany(size)
+        if not rows:
+            break
+        for row in rows:
+            yield row
