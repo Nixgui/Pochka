c8617523e4b097536182bd1de72b7ebe4283ca9b
Nixgui <robert.ecward@gmail.com>
2021-01-22 23:15:09 +0200

    Connected to MySql and execute Data

--- Own Project/main.py
+++ Own Project/main.py
@@ -1,15 +1,34 @@
 from mysql.connector import MySQLConnection, Error
 from DBconn import DBconf
-def show_client(): 
+def show_client():
+    global cursor, conn, row
     try:
         db_config=DBconf()
         conn = MySQLConnection(**db_config)
         cursor = conn.cursor()
-
-        cursor.execute("SELECT * FROM books")
+        cursor.execute("SELECT clients.ClientName, hairdresser.HairdresserName, clients.BeginData, clients.EndData "
+                       "FROM clients "
+                       "INNER JOIN hairdresser ON clients.Hairdresser = hairdresser.ID")
 
         for row in iter_row(cursor, 10):
-            print(row)
+            print(row[0],row[1],row[2],row[3])
+
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+def acconts():
+    global cursor, conn, row
+    try:
+        db_config = DBconf()
+        conn = MySQLConnection(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("SELECT Username, Password FROM accounts ")
+
+        for row in iter_row(cursor, 10):
+            print(row[0], row[1])
 
     except Error as e:
         print(e)
