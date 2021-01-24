fb0b03cda6cac1027211cbd5238aa78ef44e18de
Nixgui <robert.ecward@gmail.com>
2021-01-24 12:45:26 +0200

    Added Comments

--- Own Project/main.py
+++ Own Project/main.py
@@ -1,10 +1,9 @@
-from mysql.connector import MySQLConnection, Error
-from DBconn import DBconf
+from mysql.connector import MySQLConnection, Error #импортируем модули для подключения к БД
+from DBconn import DBconf #Импортируем конфигурации для подключения в БД
 import sys
-from PySide import QtCore, QtGui
-from Beggin_1 import Ui_Window1, Ui_buttons, Ui_Data
-
-def show_client():
+from PySide import QtCore, QtGui #ипортируем GUI
+from Beggin_1 import Ui_Window1, Ui_buttons, Ui_Data #Импорт Окон
+def show_client(): #Функция для подключения и вывода строк из БД
     global cursor, conn, rows, row
     try:
         db_config=DBconf()
@@ -14,7 +13,7 @@ def show_client():
                        "FROM clients "
                        "INNER JOIN hairdresser ON clients.Hairdresser = hairdresser.ID")
 
-        for row in iter_row(cursor, 10):
+        for row in iter_row(cursor, 10): #Вывод 10 строк разом
             rows=row[0],row[1],row[2],row[3]
 
     except Error as e:
@@ -23,7 +22,7 @@ def show_client():
     finally:
         cursor.close()
         conn.close()
-def acconts():
+def acconts(): #Функция для извлечения из БД аккаунтов
     global cursor, conn, rowlp
     try:
         db_config = DBconf()
@@ -40,50 +39,49 @@ def acconts():
     finally:
         cursor.close()
         conn.close()
-def iter_row(cursor, size=10):
+def iter_row(cursor, size=10): #Функция для вывода конкретного числа строк в память
     while True:
         rows = cursor.fetchmany(size)
         if not rows:
             break
         for row in rows:
             yield row
-app = QtGui.QApplication(sys.argv)
-Window = QtGui.QWidget()
-ui = Ui_Window1()
-ui.setupUi(Window)
-Form = QtGui.QWidget()
-ui1 = Ui_buttons()
-ui1.setupUi(Form)
-Data = QtGui.QWidget()
-ui2 = Ui_Data()
-ui2.setupUi(Data)
-Window.show()
+app = QtGui.QApplication(sys.argv) #Запуск приложения
+Window = QtGui.QWidget() #Присваивание виджета к переменноей 1-го окна
+ui = Ui_Window1() #присваивание обьектов к виджету
+ui.setupUi(Window) # Указание что обьекты привязаны в Виджету (окну)
+Form = QtGui.QWidget() ##Присваивание виджета к переменноей 2-го окна
+ui1 = Ui_buttons() # Указание что обьекты привязаны в Виджету (окну)
+ui1.setupUi(Form) # Указание что обьекты привязаны в Виджету (окну)
+Data = QtGui.QWidget() ##Присваивание виджета к переменноей 3-го окна
+ui2 = Ui_Data() # Указание что обьекты привязаны в Виджету (окну)
+ui2.setupUi(Data) # Указание что обьекты привязаны в Виджету (окну)
+Window.show() #Показать Экран (ПС: в самуом низу прописана иницилезация приложения, без него показ окна не произведёться)
 
-def login():
-    acconts()
-    login=ui.LoginText.displayText()
-    password=ui.PasswordText.displayText()
+def login():# Функция на проверку совподает ли логин и пароль
+    acconts() #Функция извлечение данных из БД
+    login=ui.LoginText.displayText() #Считывание то что ввели в поле Логин
+    password=ui.PasswordText.displayText() #Считавыние поля Пароль
     if login in rowlp[0]:
         if password in rowlp[1]:
-            Window.hide()
-            Form.show()
+            Window.hide() #Скрыть окно входа
+            Form.show() #Показать окно Формы (Кнопки для выбора действий с БД)
         else:
-            ui.Error.setText("Неверный логин или пароль!")
-            ui.LoginText.clear()
-            ui.PasswordText.clear()
+            ui.Error.setText("Неверный логин или пароль!") #Вывод если пароли или логин не совпадают
+            ui.LoginText.clear() #Очистка полдя Логин
+            ui.PasswordText.clear() #Очистка поля Пароль
     else:
-        ui.Error.setText("Неверный логин или пароль!")
-        ui.LoginText.clear()
-        ui.PasswordText.clear()
-def ShowClient():
-    Form.hide()
-    Data.show()
-    show_client()
-    print(row[2])
-    ui2.label_2.setText(row[0])
-    ui2.label.setText(row[1])
-    ui2.dateTimeEdit.setDateTime(row[2])
-    ui2.dateTimeEdit_2.setDateTime(row[3])
-ui1.ShowClient.clicked.connect(ShowClient)
-ui.pushButton.clicked.connect(login)
-sys.exit(app.exec_())
+        ui.Error.setText("Неверный логин или пароль!") #Вывод если пароли или логин не совпадают
+        ui.LoginText.clear() #Очистка полдя Логин
+        ui.PasswordText.clear() #Очистка поля Пароль
+def ShowClient(): #функция для перехода к третьему окну и вывод там клиентов
+    Form.hide() #Скрыть 2-е окно
+    Data.show() #Показать 3-е окно (1-е окно скрыто до тех пор пока его не вызовут)
+    show_client() #Фукция показа клиентов
+    ui2.label_2.setText(row[0]) #Замена пустого текста на то что есть в БД
+    ui2.label.setText(row[1]) #Замента пустого текста на то что есть в БД
+    ui2.dateTimeEdit.setDateTime(row[2]) #Выставление Даты и Времени начала работы
+    ui2.dateTimeEdit_2.setDateTime(row[3]) #Выставление Даты и Времени окнчания работы
+ui1.ShowClient.clicked.connect(ShowClient) #Присвоение к кнопке ShowClient по клику ЛКМ обращение к функции ShowClient
+ui.pushButton.clicked.connect(login) #Присвоение к кнопке pushButton обращение к функции login
+sys.exit(app.exec_()) #Иницелизация приложения (Второго быть не может!)
